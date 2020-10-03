bbd7eec3ff9a6944e12b1fa76c9f364fe970361f
A.J. Ohiwerei <ajohiwerei@gmail.com>
2020-10-03 16:05:02 -0700

    Create pages

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+
+const About = () => (
+  <div>
+    <Link href="/">home</Link>
+    <div>stuff about me</div>
+  </div>
+)
+
+export default About;
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+const Top = () => (
+  <div>
+    <Link href="/about">about</Link>
+    <div>this is top page</div>
+  </div>
+)
+
+export default Top;
