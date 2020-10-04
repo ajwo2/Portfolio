be29820922c738cc48e5231c0fdca2566428d69d
A.J. Ohiwerei <ajohiwerei@gmail.com>
2020-10-04 16:42:46 -0700

    Try adding css

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link'
 
 const Top = () => (
   <div>
-    <li>
-    <Link href="/about">About</Link>
-    </li>
+    <div className="topnav">
+      <Link href="/about">About</Link>
+    </div>
     <Link href="/projects">Projects</Link>
     <Link href="/contact">Contact</Link>
     <h1>Home Page</h1>
