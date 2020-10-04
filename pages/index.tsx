889b6206536bf1d17a413fc4f3a2be74d5a9670c
A.J. Ohiwerei <ajohiwerei@gmail.com>
2020-10-04 16:05:55 -0700

    Try again to add space

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Top = () => (
   <div>
     <Link href="/about">About</Link>
-    <p> </p>
+    <span>  </span>
     <Link href="/projects">Projects</Link>
     <Link href="/contact">Contact</Link>
     <h1>Home Page</h1>
