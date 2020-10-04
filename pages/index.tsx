f64229d80644662636ef26abc2acb2c77cf29933
A.J. Ohiwerei <ajohiwerei@gmail.com>
2020-10-04 15:52:19 -0700

    Test styled h1 header

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,10 @@ import Link from 'next/link'
 
 const Top = () => (
   <div>
-    <div><Link href="/about">About</Link></div>
+    <Link href="/about">About</Link>
     <Link href="/projects">Projects</Link>
     <Link href="/contact">Contact</Link>
+    <h1 style="color:Tomato; font-family:courier">Home Page</h1>
     <div>Welcome...</div>
   </div>
 )
