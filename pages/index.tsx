0c47447bed6971dbc8b1f3966bd5f7778fc00be4
A.J. Ohiwerei <ajohiwerei@gmail.com>
2020-10-04 15:54:31 -0700

    Fix attempt

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const Top = () => (
     <Link href="/about">About</Link>
     <Link href="/projects">Projects</Link>
     <Link href="/contact">Contact</Link>
-    <h1 style="color:Tomato; font-family:courier">Home Page</h1>
+    <h1 style="color:Tomato;font-family:courier">Home Page</h1>
     <div>Welcome...</div>
   </div>
 )
