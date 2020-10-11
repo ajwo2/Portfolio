9d6b33d27af5b3c718f3852cb4487c636cff13dd
A.J. Ohiwerei <ajohiwerei@gmail.com>
2020-10-10 23:56:30 -0700

    Add contact form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link'
 import Layout from '../components/layout'
 import Nav from '../components/nav'
+import React from 'react'
+import ContactForm from '../components/ContactForm'
+
 const Top = () => (
   <div>
    <Nav/>
    <Layout>
-      Gmail
+      <ContactForm/>
     </Layout>
   </div>
 )
